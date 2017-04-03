62636bdc9941b0539aa5797b95e4c4ac87dbc709
Yalçın Yenigün <yalcin.yenigun@iyzico.com>
2017-04-03 19:37:14 +0300

    Integration test base class added.

---
+++ boot-mon-server/src/test/java/com/iyzico/bootmon/server/registration/BootmonClientServiceIT.java
@@ -0,0 +1,45 @@
+package com.iyzico.bootmon.server.registration;
+
+import com.iyzico.bootmon.server.IntegrationTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BootmonClientServiceIT extends IntegrationTest {
+
+    @Autowired
+    BootmonClientService bootmonClientService;
+
+    RedisServer redisServer;
+
+    @Before
+    public void setUp() throws IOException {
+        redisServer = new RedisServer();
+        redisServer.start();
+    }
+
+    @Test
+    public void should_persist_bootmon_client_to_redis() throws IOException {
+        BootmonClient bootmonClient = new BootmonClient();
+        bootmonClient.setName("myClient");
+        bootmonClient.setManagementUrl("http://myhost/management");
+        bootmonClient.setHealthCheckUrl("http://myhost/health");
+        bootmonClientService.saveBootmonClient(bootmonClient);
+        BootmonClient bootmonClientActual = bootmonClientService.findBootmonClienByName(bootmonClient.getName());
+        assertThat(bootmonClientActual).isNotNull();
+        assertThat(bootmonClientActual.getName()).isEqualTo(bootmonClient.getName());
+        assertThat(bootmonClientActual.getManagementUrl()).isEqualTo(bootmonClient.getManagementUrl());
+        assertThat(bootmonClientActual.getHealthCheckUrl()).isEqualTo(bootmonClient.getHealthCheckUrl());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        redisServer.stop();
+    }
+}
